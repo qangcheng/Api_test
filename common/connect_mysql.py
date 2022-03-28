c8d4ac6d6d37cc676235c238061c6588d6d089cd
qangchen <m3632721415@136.com>
2022-03-28 12:31:47 +0800

    参数可读化

--- common/connect_mysql.py
+++ common/connect_mysql.py
@@ -1,4 +1,4 @@
-import  pymysql
+import pymysql
 
 # 数据库配置信息
 db_info = {
@@ -11,14 +11,8 @@ db_info = {
 
 
 class Dbconnect(object):
-    def __init__(self, host, user, password, database, port):
-        self.db = pymysql.connect(host=host,  # 数据库地址
-                                  user=user,
-                                  password=password,
-                                  database=database,  # 需要查询表单名称
-                                  port=port,
-                                  cursorclass=pymysql.cursors.DictCursor
-                                  )
+    def __init__(self, dbinfo):
+        self.db = pymysql.connect(**dbinfo, cursorclass=pymysql.cursors.DictCursor)
         self.cursor = self.db.cursor()
 
     def select_sql(self, sql):
@@ -45,13 +39,10 @@ class Dbconnect(object):
 
 if __name__ == '__main__':
     # 测试类的方法是否可以使用
-    db = Dbconnect(host="49.235.92.12",  # 数据库地址
-                   user="root",
-                   password="123456",
-                   database="apps",  # 需要查询表单名称
-                   port=3309)
-    sql1 = 'DELETE FROM auth_user WHERE username= "test_xxyy"'
-    db.select_sql(sql1)
+    db = Dbconnect(db_info)
+    sql1 = 'SELECT * FROM auth_user WHERE username="test1";'
+    r = db.select_sql(sql1)
+    print(r)
     sql2 = 'UPDATE apiapp_goods SET goodsname ="test133" WHERE id =12 '
     db.execute_sql(sql2)
     sql3 = 'SELECT *from apiapp_goods WHERE id =12'
